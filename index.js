8dabb39dc43a1ea86123aa7dc5436c540f7174f1
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 15:14:01 +0200

    fix cors problem

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://mamairie.fr-cork.surge.sh/');
+  res.header('Access-Control-Allow-Origin', 'http://mamairie.fr-cork.surge.sh');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
