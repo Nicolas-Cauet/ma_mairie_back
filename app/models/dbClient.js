09de83f280c23acfe7a34027541bf49a512dbfcc
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 12:11:43 +0200

    modification cors origin

--- app/models/dbClient.js
+++ app/models/dbClient.js
@@ -1,12 +1,14 @@
-require("dotenv").config();
+require(`dotenv`).config();
 
-const { Pool } = require("pg");
+const { Pool } = require(`pg`);
 
 const client = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATA_BASE_URL,
+  ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
+    rejectUnauthorized: false,
+  },
 });
+
+client.connect();
 
 module.exports = client;
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require(`dotenv`).config();
 const express = require(`express`);
 const debug = require(`debug`)(`APP`);
 const session = require(`express-session`);
-
+const cors = require(`cors`);
+const CORS = process.env.CORS;
+const corsOptions = {
+  origin: [CORS],
+  optionsSuccessStatus: 200,
+};
 const router = require(`./app/routers/router`);
 
 const PORT = process.env.PORT || 3333;
@@ -11,6 +16,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(session({
   secret: `keyboard cat`,
   cookie: {},
