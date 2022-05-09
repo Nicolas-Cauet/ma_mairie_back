dfc6c131cf1c5cee0a3f7dafe076c3b5bd7f9d2d
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 15:30:13 +0200

    fix cors problem

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://mamairie.fr-cork.surge.sh"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `http://mamairie.fr-cork.surge.sh`); // update to match the domain you will make the request from
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
   next();
 });
 app.use(session({
