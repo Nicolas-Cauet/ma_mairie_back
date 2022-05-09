10d14953981c82d585dc9b5e50a3f0a1e5963466
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 13:54:47 +0200

    heroku deploy

--- app/models/dbClient.js
+++ app/models/dbClient.js
@@ -3,12 +3,11 @@ require(`dotenv`).config();
 const { Pool } = require(`pg`);
 
 const client = new Pool({
-  connectionString: process.env.DATA_BASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
     rejectUnauthorized: false,
   },
 });
-
 client.connect();
 
 module.exports = client;
