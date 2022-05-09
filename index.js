2bdd6aa62b47c1fdd2017506c574b0f1512546cd
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 14:55:08 +0200

    fix cors problem again

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require(`express`);
 const debug = require(`debug`)(`APP`);
 const session = require(`express-session`);
 const router = require(`./app/routers/router`);
-const cors = require(`cors`);
 
 const PORT = process.env.PORT || 3333;
 
@@ -11,7 +10,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://mamairie.fr-cork.surge.sh/');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+});
 app.use(session({
   secret: `keyboard cat`,
   cookie: {},
@@ -25,3 +35,4 @@ app.use(router);
 app.listen(PORT, () => {
   debug(`Listening on https://localhost:${PORT} `);
 });
+
