4885139101338fded1d4506b315eea69377af771
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-09 12:20:21 +0200

    modification cors origin

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ require(`dotenv`).config();
 const express = require(`express`);
 const debug = require(`debug`)(`APP`);
 const session = require(`express-session`);
-const cors = require(`cors`);
-const CORS = process.env.CORS;
-const corsOptions = {
-  origin: [CORS],
-  optionsSuccessStatus: 200,
-};
 const router = require(`./app/routers/router`);
 
 const PORT = process.env.PORT || 3333;
@@ -16,7 +10,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
 app.use(session({
   secret: `keyboard cat`,
   cookie: {},
