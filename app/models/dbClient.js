1830f3fd09c965e2b45347573311233e31861711
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-10 15:00:50 +0200

    fix db connection

--- app/models/dbClient.js
+++ app/models/dbClient.js
@@ -2,25 +2,25 @@ require(`dotenv`).config();
 
 const { Pool } = require(`pg`);
 
-// Work production
-// const client = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// Work localy
+Work production
 const client = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  ssl: {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
     rejectUnauthorized: false,
   },
 });
+
+// // Work localy
+// const client = new Pool({
+//   host: process.env.PGHOST,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   database: process.env.PGDATABASE,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 client.connect();
 
