7aa80917a1169fbd9dc95f748552b73a6a02bc0e
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-11 19:23:59 +0200

    tools reporting

--- app/routers/router.js
+++ app/routers/router.js
@@ -7,11 +7,22 @@ const { validateCreateAdmin } = require(`../validation/validations`);
 const authenticateToken = require(`../middleware/authenticateToken`);
 const router = express.Router();
 
+/********** ADMIN ************/
 router.post(`/signup`, validateCreateAdmin(schemaCreateAdmin), routerWrapper(adminController.signup));
 router.post(`/login`, routerWrapper(adminController.login));
-router.post(`/reporting/mairie/:id`, authenticateToken, routerWrapper(adminReportingController.allReporting));
+
+/********** REPORTING ************/
+router.get(`/admin/reporting/:town_hall_id`, authenticateToken, routerWrapper(adminReportingController.allReporting));
+router.get(`/admin/reporting/:town_hall_id/:reporting_id`, authenticateToken, routerWrapper(adminReportingController.oneReporting));
+router.delete(`/admin/reporting/:town_hall_id/:reporting_id`, authenticateToken, routerWrapper(adminReportingController.deleteReporting));
+router.put(`/admin/reporting/:town_hall_id/:reporting_id`, authenticateToken, routerWrapper(adminReportingController.modifyReporting));
 
 // route de test
 router.get(`/admin`, authenticateToken, routerWrapper(adminController.isConnect));
+
+/********** VISITEUR ************/
+/********** REPORTING ************/
+router.get(`/admin/reporting/:town_hall_id`, authenticateToken, routerWrapper(adminReportingController.allReporting));
+router.post(`/reporting/:town_hall_id`, routerWrapper(adminReportingController.postReporting));
 
 module.exports = router;
