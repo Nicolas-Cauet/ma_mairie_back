f1bf69d498ed94e8401701faef9d2873237cdc43
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-12 14:25:40 +0200

    fix cors adress

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept');
+  res.header(`Access-Control-Allow-Origin`, `https://ma-mairie.surge.sh/`);
+  res.header(`Access-Control-Allow-Methods`, `GET, POST, OPTIONS, PUT, PATCH, DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Authorization, Content-Type, Accept`);
   next();
 });
 
