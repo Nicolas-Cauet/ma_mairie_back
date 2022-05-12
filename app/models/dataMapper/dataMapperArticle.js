bf2ca089e16dd5d021951789e5cba6aea1ca7ff5
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-12 17:57:32 +0200

    erreur bdd local/distant

--- app/models/dataMapper/dataMapperArticle.js
+++ app/models/dataMapper/dataMapperArticle.js
@@ -2,6 +2,15 @@ const client = require(`../dbClient`);
 
 const dataMapperArticle = {
 
-}
+  async getAllArticle(townHallId) {
+    const query = {
+      text: `SELECT * FROM reporting
+                    WHERE town_hall_id = $1;`,
+      values: [townHallId],
+    };
+    const data = await client.query(query);
+    return data.rows;
+  },
+};
 
 module.exports = dataMapperArticle;
