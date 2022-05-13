9757cac74a6e03993ae3d0f211661ca759af44a7
LegrainAnthony <legrain.anthony51@gmail.com>
2022-05-13 11:58:11 +0200

    console.log

--- app/models/dataMapper/dataMapperArticle.js
+++ app/models/dataMapper/dataMapperArticle.js
@@ -1,7 +1,15 @@
 const client = require(`../dbClient`);
 
 const dataMapperArticle = {
-
-}
+  async getAllArticleAdmin(townHallId) {
+    const query = {
+      text: `SELECT * FROM article
+            WHERE town_hall_id = $1;`,
+      values: [townHallId],
+    };
+    const data = await client.query(query);
+    return data.rows;
+  },
+};
 
 module.exports = dataMapperArticle;
