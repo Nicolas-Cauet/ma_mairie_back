a54dd55aab29101fe821301391691ab56d86207a
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-15 14:11:07 +0200

    joi schema on reporting user route

---
+++ app/validation/schema/schemaCreateReportingUser.js
@@ -0,0 +1,15 @@
+/* eslint-disable prefer-regex-literals */
+/* eslint-disable no-useless-escape */
+const joi = require(`joi`);
+
+const schemaCreateReportingUser = joi.object({
+  reporting_category: joi.string().required(),
+  title: joi.string().min(5).max(20).required(),
+  user_text: joi.string().required(),
+  email: joi.string().email().required().max(20),
+  first_name: joi.string().required().max(20),
+  last_name: joi.string().required.max(20),
+  phone_number: joi.string().min(10).max(10),
+});
+
+module.exports = schemaCreateReportingUser;
