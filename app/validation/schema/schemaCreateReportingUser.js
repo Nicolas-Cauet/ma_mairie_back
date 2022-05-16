dd56aa0c88234f8d2bb3697fefb2cf35c0b62153
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-16 15:53:53 +0200

    fix all bugs merge commit main

--- app/validation/schema/schemaCreateReportingUser.js
+++ app/validation/schema/schemaCreateReportingUser.js
@@ -8,21 +8,9 @@ const schemaCreateReportingUser = joi.object({
   user_text: joi.string().required(),
   email: joi.string().email().required(),
   first_name: joi.string().required().max(20),
-<<<<<<< HEAD
-<<<<<<< HEAD
   last_name: joi.string().required.max(20),
   phone_number: joi.string().min(10).max(10),
   town_hall_id: joi.string(),
-=======
-  last_name: joi.string().required().max(20),
-  phonenumber: joi.string().min(10).max(10),
-  town_hall_id: joi.number(),
->>>>>>> reporting
-=======
-  last_name: joi.string().required().max(20),
-  phonenumber: joi.string().min(10).max(10),
-  town_hall_id: joi.number(),
->>>>>>> 5853bdf2793af3884d53a2c28032ce5236f62d86
 });
 
 module.exports = schemaCreateReportingUser;
