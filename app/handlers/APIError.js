ff2ad636615ac9218926eccc424fba06006c3714
Nicolas Cauet <51637140+Nicolas-Cauet@users.noreply.github.com>
2022-05-16 15:29:37 +0200

    Update APIError.js

--- app/handlers/APIError.js
+++ app/handlers/APIError.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const { appendFile } = require("fs/promises");
-
 const path = require(`path`);
-=======
 const debug = require(`debug`)(`APIERROR`);
->>>>>>> reporting
 
-=======
 /**
  * @type {class}
  * @namespace APIError
  * @exports APIError
  */
->>>>>>> 5853bdf2793af3884d53a2c28032ce5236f62d86
 class APIError extends Error {
   constructor(message, url, status = 500) {
     super(message); // super appelle le constructeur du parent
@@ -22,14 +15,11 @@ class APIError extends Error {
   }
 
   /**
-<<<<<<< HEAD
    * Méthode pour logger les erreurs
    * @param {string} message d'erreur
    * @returns
-=======
    * The method returns an error message in console
    * @returns void
->>>>>>> 5853bdf2793af3884d53a2c28032ce5236f62d86
    */
   async log() {
     // Gestion de l'affichage de l'erreur dans la console - instantanéité
@@ -39,8 +29,6 @@ class APIError extends Error {
       date: new Date(),
     };
     console.error(this.url, this.message, new Date());
-<<<<<<< HEAD
-<<<<<<< HEAD
     // Gestion des fichiers de log - historique
     const logPath = path.resolve(__dirname);
     const fileName = `${new Date().toISOString().split(`T`)[0]}.csv`;
@@ -51,12 +39,8 @@ class APIError extends Error {
       `${logPath}/${fileName}`,
       `${new Date().toLocaleTimeString()},${this.url},${this.message}\n`
     );
-=======
     debug(error);
     return error;
->>>>>>> reporting
-=======
->>>>>>> 5853bdf2793af3884d53a2c28032ce5236f62d86
   }
 }
 
