fce287902f04c116a722a9d8b77c9eb64cabbda6
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-16 16:21:57 +0200

    fix all bugs git main

--- app/routers/router.js
+++ app/routers/router.js
@@ -74,7 +74,7 @@ router.post(
 
 router.get(`/council/:town_hall_id`, authenticateToken, routerWrapper(adminControllerCouncil.allCouncil));
 
-router.use((req, next) => {
+router.use((req, res, next) => {
   next(new APIError(`Url demander n'est pas valide !`, req.url, 404));
 });
 
