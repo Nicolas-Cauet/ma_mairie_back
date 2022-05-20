1ab40ce5ba60a70ef2a732712887caa125de1e2f
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-20 09:47:00 +0200

    fix cors connexion

--- app/models/dbClient.js
+++ app/models/dbClient.js
@@ -9,21 +9,21 @@ const { Pool } = require(`pg`);
  * @export client
  * @class Pool
  */
-// const client = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
-//     rejectUnauthorized: false,
-//   },
-// });
+const client = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { // on demande d'accepter le fait qu'on ne soit pas en ssl (sécurité)
+    rejectUnauthorized: false,
+  },
+});
 
 // Local conenxion
 
-const client = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-});
+// const client = new Pool({
+//   host: process.env.PGHOST,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+// });
 
 client.connect();
 
