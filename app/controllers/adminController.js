2e594c695e06b0e09cd6caac8bf7848641a0af55
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-25 09:53:57 +0200

    fix cors problem

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -2,7 +2,6 @@ require(`dotenv`).config();
 const bcrypt = require(`bcrypt`);
 const jwt = require(`jsonwebtoken`);
 const secretKey = process.env.ACCES_TOKEN_SECRET;
-const debug = require(`debug`)(`CONTROLLER`);
 const { dataMapperAdmin } = require(`../models/dataMapper/index`);
 
 /**
@@ -27,22 +26,17 @@ const adminController = {
     const hashPassword = await bcrypt.hash(req.body.password, 10);
     const townHallId = await dataMapperAdmin.getTownHallId(req.body.insee);
     const existingUser = await dataMapperAdmin.getOneAdmin(req.body.email);
-    if (!existingUser) {
-      const userSignup = await dataMapperAdmin
-        .userSignup(req.body.pseudo, req.body.insee, hashPassword, req.body.email, townHallId);
-      res.status(200).send(`Votre compte a bien été créé, vous pouvez vous connecter.`);
-      if (!userSignup.rowCount) {
-        const err = new Error(`La connexion a échoué vérifier vos données !`);
-        next(err);
-      }
-    }
-    if (existingUser.pseudo === req.body.pseudo) {
-      const err = new Error(`Le pseudo est déjà prit merci d'en saisir un autre !`);
-      next(err);
-    } else if (existingUser.email === req.body.email) {
-      const err = new Error(`Adresse email est déjà prise merci d'en saisir une autre !`);
+    if (existingUser) {
+      const err = new Error(`L'utilisateur existe déja`);
       next(err);
     }
+    const userSignup = await dataMapperAdmin
+      .userSignup(req.body.pseudo, req.body.insee, hashPassword, req.body.email, townHallId);
+    if (!userSignup.rowCount) {
+      const err = new Error(`La connexion a échoué vérifier vos données !`);
+      next(err);
+    }
+    res.status(200).send(`L'utilisateur est bien enregistré en base !`);
   },
   /**
    * The method allows you to log in as an administrator
@@ -55,7 +49,7 @@ const adminController = {
   async login(req, res, next) {
     const existingUser = await dataMapperAdmin.getOneAdmin(req.body.email);
     if (!existingUser) {
-      const err = new Error(`Adresse e-mail saisie n'existe pas !`);
+      const err = new Error(`Impossible de récupérer Administrateur en base !`);
       next(err);
     }
     const match = await bcrypt.compare(req.body.password, existingUser.password);
@@ -66,7 +60,7 @@ const adminController = {
       const accessToken = jwt.sign(user, secretKey);
       res.json({ accessToken, townHallId });
     } else {
-      const err = new Error(`Votre mail et/ou mot de passe sont incorrect !`);
+      const err = new Error(`La connexion a échoué vérifier vos données !`);
       next(err);
     }
   },
