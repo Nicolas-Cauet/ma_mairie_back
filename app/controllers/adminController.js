35158e6958e8386385acc99c9dfc106aeee7a94e
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-25 10:23:03 +0200

    fix message error

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -18,7 +18,12 @@ const adminController = {
    * @returns void
    */
   async signup(req, res, next) {
-    if (req.body.pseudo === `` || req.body.insee === `` || req.body.password === `` || req.body.email === ``) {
+    if (
+      req.body.pseudo === `` ||
+      req.body.insee === `` ||
+      req.body.password === `` ||
+      req.body.email === ``
+    ) {
       const err = new Error(`Merci de saisir tous les champs !`);
       err.status = 406;
       next(err);
@@ -26,16 +31,30 @@ const adminController = {
     const hashPassword = await bcrypt.hash(req.body.password, 10);
     const townHallId = await dataMapperAdmin.getTownHallId(req.body.insee);
     const existingUser = await dataMapperAdmin.getOneAdmin(req.body.email);
-    if (existingUser.pseudo === req.body.pseudo) {
-      const err = new Error(`Le pseudo est déjà prit merci d'en saisir un autre !`);
-      next(err);
-    } else if (existingUser.email === req.body.email) {
-      const err = new Error(`Adresse email est déjà prise merci d'en saisir une autre !`);
-      next(err);
-    } else if (!existingUser) {
-      const userSignup = await dataMapperAdmin
-        .userSignup(req.body.pseudo, req.body.insee, hashPassword, req.body.email, townHallId);
-      res.status(200).send(`Votre compte a bien été créé, vous pouvez vous connecter.`);
+    if (existingUser) {
+      if (existingUser.pseudo === req.body.pseudo) {
+        const err = new Error(
+          `Le pseudo est déjà prit merci d'en saisir un autre !`,
+        );
+        next(err);
+      } else if (existingUser.email === req.body.email) {
+        const err = new Error(
+          `Adresse email est déjà prise merci d'en saisir une autre !`,
+        );
+        next(err);
+      }
+    }
+    if (!existingUser) {
+      const userSignup = await dataMapperAdmin.userSignup(
+        req.body.pseudo,
+        req.body.insee,
+        hashPassword,
+        req.body.email,
+        townHallId,
+      );
+      res
+        .status(200)
+        .send(`Votre compte a bien été créé, vous pouvez vous connecter.`);
       if (!userSignup.rowCount) {
         const err = new Error(`La connexion a échoué vérifier vos données !`);
         next(err);
@@ -56,9 +75,15 @@ const adminController = {
       const err = new Error(`Impossible de récupérer Administrateur en base !`);
       next(err);
     }
-    const match = await bcrypt.compare(req.body.password, existingUser.password);
+    const match = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
     if (match) {
-      const data = await dataMapperAdmin.userLogin(req.body.email, existingUser.password);
+      const data = await dataMapperAdmin.userLogin(
+        req.body.email,
+        existingUser.password
+      );
       const user = { pseudo: data.pseudo, town_hall_id: data.town_hall_id };
       const townHallId = user.town_hall_id;
       const accessToken = jwt.sign(user, secretKey);
