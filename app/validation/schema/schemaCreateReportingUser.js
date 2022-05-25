6538ce3ff5ddb338dd94af553b7488f1a5d51f56
Nicolas Cauet <cauet.nicolas@gmail.com>
2022-05-25 15:46:11 +0200

    change required schema joi

--- app/validation/schema/schemaCreateReportingUser.js
+++ app/validation/schema/schemaCreateReportingUser.js
@@ -4,10 +4,10 @@
 const joi = require(`joi`);
 
 const schemaCreateReportingUser = joi.object({
-  reporting_category: joi.string().required().label(`la categorie du signalement est requise !`),
+  reporting_category: joi.string().label(`la categorie du signalement est requise !`).required(),
   title: joi.string().min(5).max(30).label(`le titre du signalement est requis !`).required(),
-  user_text: joi.string().required().label(`le texte du signalement est requis !`),
-  email: joi.string().email().required().label(`Votre email dans le signalement est requis !`),
+  user_text: joi.string().label(`le texte du signalement est requis !`).required(),
+  email: joi.string().email().label(`Votre email dans le signalement est requis !`).required(),
   first_name: joi.string().required().max(20).label(`le Prénom dans le signalement est requis !`),
   last_name: joi.string().required().max(20).label(`le Nom de famille dans le signalement est requis !`),
   phonenumber: joi.string().min(10).max(10).allow(``),
